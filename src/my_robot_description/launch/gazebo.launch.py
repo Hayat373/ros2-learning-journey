70781e939c2bd09774c3cca099978796e0e1e5eb
Hayat373 <hayahmam3@gmail.com>
2026-07-23 17:19:26 +0300

    controler

--- src/my_robot_description/launch/gazebo.launch.py
+++ src/my_robot_description/launch/gazebo.launch.py
@@ -8,19 +8,19 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
     pkg_share = get_package_share_directory('my_robot_description')
     urdf_file = os.path.join(pkg_share, 'urdf', 'simple_robot.urdf')
-
-    # Gazebo launch
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            get_package_share_directory('ros_gz_sim'),
-            '/launch/gz_sim.launch.py'
-        ]),
-        launch_arguments={'gz_args': '-r empty.sdf'}.items()
-    )
+    controllers_file = os.path.join(pkg_share, 'config', 'diff_controller.yaml')
 
     return LaunchDescription([
-        gazebo,
+        # Gazebo
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                get_package_share_directory('ros_gz_sim'),
+                '/launch/gz_sim.launch.py'
+            ]),
+            launch_arguments={'gz_args': '-r empty.sdf'}.items()
+        ),
 
+        # Robot State Publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -29,11 +29,27 @@ def generate_launch_description():
             parameters=[{'robot_description': open(urdf_file).read()}]
         ),
 
-        # Spawn robot in Gazebo
+        # Spawn robot
         Node(
             package='ros_gz_sim',
             executable='create',
             arguments=['-topic', '/robot_description', '-name', 'simple_robot'],
+            output='screen'
+        ),
+
+        # ros2_control node
+        Node(
+            package='controller_manager',
+            executable='ros2_control_node',
+            parameters=[{'robot_description': open(urdf_file).read()}, controllers_file],
+            output='screen'
+        ),
+
+        # Load controller
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['diff_drive_controller', '--controller-ros-args', '--activate'],
             output='screen'
         ),
     ])
